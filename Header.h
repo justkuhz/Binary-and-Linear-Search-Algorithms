6a09681ab6a05bf0da54c93bd96427ad188ebabf
Ken Zhu <k-zhu@onu.edu>
2022-10-11 23:05:57 -0400

    project code portion completed

--- Header.h
+++ Header.h
@@ -29,27 +29,56 @@ vector<int> randomVector(int n) {
 	vector<int> vec(n);
 	generate(vec.begin(), vec.end(), gen);
 
-	for (auto i : vec) {
-		cout << i << " ";
-	}
 	return vec;
 }
 
-/*
-*/
-void mergeSort(vector<int>& v) {
 
+/*
+* A binary search algorithm. We take a sorted algorithm and cut the search area by half until we find the element
+* or find that the element does not exist within the vector.
+* 
+* Prints out the number of comparisons or checks that are made against the key.
+* 
+* @param v - sorted vector of elements to search through.
+* @param key - the key we are searching for within vector v.
+* @return the index of the element matching key, otherwise -1 if key is not found.
+*/
+int binarySearch(vector<int> v, int key) {
+	int pivot, left = 0, right = v.size() - 1, comp = 0;
+
+	while (left <= right) {
+
+		pivot = left + (right - left) / 2;
+
+		comp++;
+		if (v[pivot] == key) return pivot;
+		comp++;
+		if (key > v[pivot]) left = pivot + 1;
+		else right = pivot - 1;
+	}
+
+	cout << "comparisons made: " << comp << endl;
+	return -1;
 }
 
 /*
+* A linear search algorithm. We traverse through all elements of the array checking if the element at
+* index i is equal to the key. If we find a match, return index i. Otherwise, return -1.
+* Prints the number of comparisons made.
+* 
+* @param v - vector of elements to search through.
+* @param key - the key we are searching for within vector v.
+* @return the index of the element matching key, otherwise -1 if key is not found.
 */
-int binarySearch(vector<int> v) {
-
-}
-
-/*
-*/
-int linearSearch(vector<int> v) {
-
-
+int linearSearch(vector<int> v, int key) {
+	int comp = 0;
+	for (int i = 0; i < v.size(); i++) {
+		comp++;
+		if (v[i] == key) {
+			cout << "comparisons made: " << comp << endl;
+			return i;
+		}
+	}
+	cout << "comparisons made: " << comp << endl;
+	return -1;
 }
--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main() {
 
-	vector<int> v = randomVector(1000);
+	int n = 100; // size of vector
+
+	// generate random vectors
+	vector<int> v1 = randomVector(n); // used for binary search
+	vector<int> v2 = randomVector(n); // used for linear search
+
+	// key to search for (we are making sure the key cannot exist in the range to force worst case)
+	int key = 3 * n + 1;
+
+	// sort vector 1, track the time to sort one of the vectors
+	auto start = chrono::steady_clock::now();
+	sort(v1.begin(), v1.end());
+	auto end = chrono::steady_clock::now();
+	chrono::duration<double> elapsed_seconds = end - start;
+	cout << "quicksort sorting time: " << elapsed_seconds.count() << " (s)\n\n";
+
+	// binary search
+	cout << "Binary Search: " << endl;
+	start = chrono::steady_clock::now();
+	cout << "Key found: " << binarySearch(v1, key) << endl;
+	end = chrono::steady_clock::now();
+	elapsed_seconds = end - start;
+	cout << "time: " << elapsed_seconds.count() << " (s)\n\n";
+
+	// linear search
+	cout << "Linear Search: " << endl;
+	start = chrono::steady_clock::now();
+	cout << "Key found: " << linearSearch(v2, key) << endl;
+	end = chrono::steady_clock::now();
+	elapsed_seconds = end - start;
+	cout << "time: " << elapsed_seconds.count() << " (s)\n";
+
 	return 0;
 }
