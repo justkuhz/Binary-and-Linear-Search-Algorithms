4aaf169330fad0f7fdb3eef47a3fa92a0680ec65
Ken Zhu <k-zhu@onu.edu>
2022-10-12 12:02:22 -0400

    new functions and modified comparison

--- Header.h
+++ Header.h
@@ -45,20 +45,18 @@ vector<int> randomVector(int n) {
 * @param key - the key we are searching for within vector v.
 * @return the index of the element matching key, otherwise -1 if key is not found.
 */
-int binarySearch(vector<int> v, int key) {
-	int pivot, left = 0, right = v.size() - 1, comp = 0;
+int binarySearch(vector<int> v, int key, int& comp) {
+	int pivot, left = 0, right = v.size() - 1;
 
 	while (left <= right) {
 
 		pivot = left + (right - left) / 2;
-
 		comp++;
 		if (v[pivot] == key) return pivot;
 		if (key > v[pivot]) left = pivot + 1;
 		else right = pivot - 1;
 	}
 
-	cout << "comparisons made: " << comp << endl;
 	return -1;
 }
 
@@ -71,15 +69,30 @@ int binarySearch(vector<int> v, int key) {
 * @param key - the key we are searching for within vector v.
 * @return the index of the element matching key, otherwise -1 if key is not found.
 */
-int linearSearch(vector<int> v, int key) {
-	int comp = 0;
+int linearSearch(vector<int> v, int key, int& comp) {
 	for (int i = 0; i < v.size(); i++) {
 		comp++;
 		if (v[i] == key) {
-			cout << "comparisons made: " << comp << endl;
 			return i;
 		}
 	}
-	cout << "comparisons made: " << comp << endl;
 	return -1;
+}
+
+/*
+* A helper function we use to display the results of our search function.
+* Prints out the type of search algorithm used, the results of the search algorithm, the comparisons made,
+* and the time it took to produce a result from the search.
+* 
+* @param search - type of search algorithm
+* @param keyFound - result of search algo
+* @param comp - comparisons made during search algo
+* @param time - total time elapsed during search algo
+*/
+void printResults(string search, int keyFound, int comp, chrono::duration<double> time) {
+	printf("%s\n", search.c_str());
+	if (keyFound >= 0) printf("Key Found at index: %d\n", keyFound);
+	else printf("Key was not found\n");
+	printf("Comparisons made: %d\n", comp);
+	cout << "Search time: " << time.count() << "(s)\n\n";
 }
--- Source.cpp
+++ Source.cpp
@@ -13,28 +13,29 @@ int main() {
 	// key to search for (we are making sure the key cannot exist in the range to force worst case)
 	int key = 3 * n + 1;
 
-	// sort vector 1, track the time to sort one of the vectors
+	// tracking comparisons made
+	int BScomp = 0, LScomp = 0;
+
+	// sort vector 1, track the time to sort to use for binary search
 	auto start = chrono::steady_clock::now();
 	sort(v1.begin(), v1.end());
 	auto end = chrono::steady_clock::now();
 	chrono::duration<double> elapsed_seconds = end - start;
-	cout << "quicksort sorting time: " << elapsed_seconds.count() << " (s)\n\n";
+	cout << "Quicksort sorting time: " << elapsed_seconds.count() << " (s)\n\n";
 
 	// binary search
-	cout << "Binary Search: " << endl;
 	start = chrono::steady_clock::now();
-	cout << "Key found: " << binarySearch(v1, key) << endl;
+	int bs = binarySearch(v1, key, BScomp);
 	end = chrono::steady_clock::now();
 	elapsed_seconds = end - start;
-	cout << "time: " << elapsed_seconds.count() << " (s)\n\n";
+	printResults("Binary Search", bs, BScomp, elapsed_seconds);
 
 	// linear search
-	cout << "Linear Search: " << endl;
 	start = chrono::steady_clock::now();
-	cout << "Key found: " << linearSearch(v2, key) << endl;
+	int ls = linearSearch(v2, key, LScomp);
 	end = chrono::steady_clock::now();
 	elapsed_seconds = end - start;
-	cout << "time: " << elapsed_seconds.count() << " (s)\n";
-
+	printResults("Linear Search", ls, LScomp, elapsed_seconds);
+	
 	return 0;
 }
